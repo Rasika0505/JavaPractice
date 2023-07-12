32ef6c0b64c307e050138898b44262ae0e1fa908
Rasika0505 <rasikasambare50@gmail.com>
2023-07-12 18:21:14 +0530

    First GIT Commit for Java

---
+++ src/JavaProgram/DataTypes.java
@@ -0,0 +1,19 @@
+package JavaProgram;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+	      int a=10;
+	      double b=20.5;
+	      char c='H';
+	      boolean d=true;
+	      String s="Welcome";
+	      
+	      System.out.println(a);
+	      System.out.println(b);
+	      System.out.println(c);
+	      System.out.println(d);
+	      System.out.println(s);
+	}
+
+}
---
+++ src/JavaProgram/Operators.java
@@ -0,0 +1,28 @@
+package JavaProgram;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		System.out.println(a+b);
+		System.out.println("sum of a and b is:"+(a+b));
+		System.out.println("diff of a and b is:"+(a-b));
+		System.out.println("mul of a and b is:"+(a*b));
+		System.out.println("div of a and b is:"+(a/b));
+		System.out.println("mod div of a and b is:"+(a%b));
+		
+		//relational operators- it always return boolean values like true and false
+		 
+		System.out.println(a==b);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+			
+
+	}
+
+}
